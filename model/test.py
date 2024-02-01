79aa1315491ad2c5e32d7467240fb39f314bb356
Ebbe <59743608+marco-borsato@users.noreply.github.com>
2024-02-01 13:15:07 +0100

    fixed pc specific path to rain csv file

--- model/test.py
+++ model/test.py
@@ -2,4 +2,9 @@ import pandas as pd
 
 dataFrame = pd.read_csv(r"C:\Users\vandi\OneDrive\Bureaublad\ABM\input_data\houston_rain_data.csv", on_bad_lines='skip', delimiter=",")
 
-print(dataFrame)
+rainfall = dataFrame['rainfall']
+amount_heiher_3 =  rainfall[rainfall > 3].count()
+print(amount_heiher_3)
+fraction = amount_heiher_3/len(rainfall)
+print(fraction*100)
+
